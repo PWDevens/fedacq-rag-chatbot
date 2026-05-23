19becb91c0fe75d9fed04b62952b51a5c4dc2b71
PWDevens <p.w.devens@gmail.com>
2026-05-23 13:10:16 -0400

    Updated Flask for local deployment

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from .config import get_config
+
+def create_app(config_name="default"):
+    """
+    Application factory pattern.
+    Allows different configs for local/dev/prod.
+    """
+    app = Flask(__name__)
+    app.config.from_object(get_config(config_name))
+
+    # Import and register routes
+    from .api import api_bp
+    app.register_blueprint(api_bp)
+
+    return app
+
+# For convenience when running locally:
+app = create_app()
