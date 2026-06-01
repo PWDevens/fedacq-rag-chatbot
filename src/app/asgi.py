d123f7b393806482f6bcc3c7ccc368142b78de22
PWDevens <p.w.devens@gmail.com>
2026-06-01 03:15:35 -0400

    resolved WSGI app with ASGI middleware package load

--- src/app/asgi.py
+++ src/app/asgi.py
@@ -1,8 +1,8 @@
 from app import create_app
-from hypercorn.middleware import wsgi
+from hypercorn.middleware import AsyncioWSGIMiddleware
 
 # Create the Flask WSGI app
 flask_app = create_app()
 
 # Wrap it as an ASGI app for Hypercorn
-app = wsgi.WSGIMiddleware(flask_app)
+app = AsyncioWSGIMiddleware(flask_app)
